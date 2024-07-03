4c871f88e64c7d05417c64b07180c0e5939425b8
xiaobaidadada <1643220580@qq.com>
2024-07-04 03:10:23 +0800

    构建脚本

--- shell/config/webpack.server.config.js
+++ shell/config/webpack.server.config.js
@@ -36,7 +36,7 @@ module.exports = {
             'process.env.NODE_ENV': JSON.stringify('production'),
         }),
         new webpack.IgnorePlugin({
-            resourceRegExp: /^\.\/win-process.*?$/,
+            resourceRegExp: /^\.\/.*?\.node$/
             // contextRegExp: /moment$/,
         })
     ],
