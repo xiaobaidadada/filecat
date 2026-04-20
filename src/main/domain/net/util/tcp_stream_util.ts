12e2d28fdbf060893ba15cb3643a862c90da4f38
xiaobaidadada <filecat@qq.com>
2026-04-20 16:51:09 +0800

    fix:变量

--- src/main/domain/net/util/tcp_stream_util.ts
+++ src/main/domain/net/util/tcp_stream_util.ts
@@ -51,7 +51,7 @@ export class tcp_stream_util {
     }
 
     public remove_on_close(close:()=>void) {
-        this.on_close_list = this.on_close_list.filter(close => close !== close);
+        this.on_close_list = this.on_close_list.filter(v => v !== close);
     }
 
     // 设置包处理函数 可以添加多个
